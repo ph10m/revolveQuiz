2d3f36cce6067147dc5a0571456c4c596f000edd
Tollef Jørgensen <tollefj@gmail.com>
2016-01-07 08:17:15 +0100

    javascript optimization

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var quiz = [
-	{question: 'Question 1 text', answers: ['12.48', '34.1', '85.3'], correct: 0},
-	{question: 'Question 2 text', answers: ['ab', 'dfwqe', 'wjeiw', 'xasd'], correct: 3},
-	{question: 'Question 3 text', answers: ['123', '456', '789'], correct: 2}
-];
+	{question: 'What is the right file format for the Cost Report BOM upload?', answers: ['OpenOffice *.sxc file', 'Microsoft Excel *.xls file', 'Adobe Acrobat *.pdf file'], correct: 1},
+	{question: 'The technical inspectors (scrutineers) do not have the right to...', answers: ['assign scoring points for passing technical inspection.', 'require a team to correct a problem, if any part of a vehicle does not comply with the rules or is otherwise deemed to be a concern.', 're-inspect any vehicle at any time during the competition.'], correct: 0},
+	{question: 'What is the maximum allowed permanent deflection of the anti intrusion plate?', answers: ['none', '25.1 mm', '15.5 mm'], correct: 2},
+	{question: 'If the submitted SEF does not pass inspection and the scrutineers request further information, teams have...', answers:['10 days from the date of the request to submit the requested information', '20 days from the date of the request to submit the requested information', 'to present the requested information during technical inspection at the event'], correct: 0},
+	{question: 'Which angle will not specifically be tested on the Tilt Table?', answers:['60.0', '52.5', '45.0'], correct: 0},
+	
+
+];//{question: 'asd', answers:['1', '2', '3'], correct: 0},
+
+
+
 
 function shuffle(array) { //fisher-yates shuffle
     var counter = array.length, temp, index;
@@ -19,26 +26,22 @@ function shuffle(array) { //fisher-yates shuffle
     }
     return array;
 }
-
-
 function get(x){
 	return document.getElementById(x);
 }
-$("input:checkbox").on('click', function() {
-  // in the handler, 'this' refers to the box clicked on
-  var $box = $(this);
-  if ($box.is(":checked")) {
-    // the name of the box is retrieved using the .attr() method
-    // as it is assumed and expected to be immutable
-    var group = "input:checkbox[name='" + $box.attr("name") + "']";
-    // the checked state of the group/box on the other hand will change
-    // and the current value is retrieved using .prop() method
-    $(group).prop("checked", false);
-    $box.prop("checked", true);
-  } else {
-    $box.prop("checked", false);
-  }
-});
+
+function generateLists(){
+	for (i=0; i<quiz.length; i++){
+		var num = String(i+1)
+		//var list_text = "<header class='header'><p class='number'>#"+num+'<h1 id=Q'+num+"></h1></header><section class='content'><ul class='list' id='q_"+num+"></ul></section>";
+		var list_text = "<header class='header'><p class='number'>#"+num+'<h1 id=Q'+num+"></h1></header><section class='content'><ul class='list' id=q_"+num+"></ul></section>";
+		$(insertList).append(list_text);
+		var list_text = "<header class='header'><p class='number'>#"+num+'<h1 id=Q'+num+"></h1></header><section class='content'><ul class='list' id='q_"+num+"></ul></section>";
+	}
+}
+generateLists();
+
+
 function setQuestions(){
 	//shuffle questions and answers
 	var currentPos= 0; //set current question
@@ -62,5 +65,25 @@ function setQuestions(){
 }
 setQuestions();
 
+
+function hover() {
+  $(".button").on("mouseenter", function() {
+    return $(this).addClass("hover");
+  });
+}
+function hoverOff() {
+  $(".button").on("mouseleave", function() {
+    return $(this).removeClass("hover");
+  });
+}
+function active() {
+  $(".button").on("click", function() {
+    return $(this).addClass("active");
+  });
+}
+
+hover();
+hoverOff();
+active();
 
 
