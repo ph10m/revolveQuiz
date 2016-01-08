f19981904efed57136059b16f1b92024302aa4f2
Tollef Jørgensen <tollefj@gmail.com>
2016-01-08 17:47:31 +0100

    final day update

--- quizzes.js
+++ quizzes.js
@@ -1,3 +1,70 @@
+//QUIZ 1
+var testOne = [
+	{question: 'Et fotballlag har vunnet 50% av de 20 siste kampene de har spilt så langt. Hvor mange kamper på rad må de vinne for å øke seierprosenten til 60%?', answers: ['5','8', '9', '6', '7', '10', '3', '4'], correct: 0},
+		
+	{question: 'Hvis du kjører til butikken i 20 km/t og tilbake i 30 km/t, hva er snitthastigheten din?', answers: ['24 km/t', '25 km/t', '26 km/t', '23 km/t','27 km/t', '22 km/t'], correct: 0},
+	
+	{question: 'Hva er vinkelsummen i en femkant?', answers: ['540 grader', '720 grader', '360 grader', '550 grader', '600 grader'], correct: 0},
+
+	{question: 'Hvilke to faser er det i duplex-stål', answers:['Austenitt & Ferritt', 'Ferritt & Martensitt', 'Bainitt & Perlitt', 'Bainitt & Martensitt', 'Perlitt & Ferritt', 'Sementitt & Austenitt'], correct: 0},
+	
+	{question: 'Hva er du i ferd med å kjøpe hvis man oppgir et DN og et PN nummer?', answers:['Rør', 'Flatstål', 'Bolt', 'Skruer', 'Bjelke'], correct: 0},
+	
+	{question: 'Hva menes med standardene 304L og 316L?', answers:['Rustfritt stål', 'Herdet stål', 'Aluminium', 'Svartstål', 'Titan'], correct: 0},
+
+	{question: 'Hva heter det i fysikken når en væske blir til gass som følge av at trykket i væsken synker til under damptrykket?', answers: ['Kavitasjon', 'Barotropi', 'Konveksjon', 'Adveksjon', 'Termal sirkulasjon'], correct: 0},
+	
+	{question: 'Hvilken spenning markerer skillet mellom elastisk og plastisk deformasjon?', answers: ['Flytespenning', 'Bøyespenning', 'Skjærspenning', 'Bruddspenning'], correct: 0},
+
+	{question: 'Hvis en 2000-watts vannkoker står på i 15 minutter. Hvor mange kilowattimer har den da brukt? Svar i kWh', answers:['0.5', '1', '0.8', '1.5', '0.3', '1.3'], correct: 0},
+	
+	{question: 'Hvilke av følgende er ikke et tegnsett?', answers:['IEEE', 'UTF-16', 'ASCII', 'UTF-8'], correct: 0},
+	
+	{question: 'Hva er navnet på en applikasjon som gjør om programmeringsspråk til et maskinspråk', answers:['Kompilator', 'Aritmetikk', 'Omformer', 'Interpreter'], correct: 0},
+	
+	{question: 'Hvor mange kvadratmeter er det i et mål?', answers:['1000', '100000', '100', '10000', '10'], correct: 0},
+	
+	{question: 'Hva skjer med en stålbjelke som varmes opp med forskjellig temperatur på over- og undersiden? Den...', answers:['Forlenges og bøyes', 'Forelenges', 'Krymper og bøyes', 'Krymper', 'Bøyes'], correct: 0},
+	
+	{question: 'Hvilken trigonometrisk funksjon beskriver forholdet mellom den hosliggende kateten og hypotenusen i en rettvinklet trekant', answers:['Cosinus', 'Arccosinus', 'Tangens', 'Arctanges', 'Sinus', 'Arcsinus'], correct: 0},
+	
+	{question: 'Hva er uttrykket for effektiviteten til en Carnot-varmemaskin?', answers:['1-(T_h/T_c)', '1/(T_c/T_h)', '1-(T_c/T_h)', '(T_h/T_c)-1', '1/(T_h/T_c)'], correct: 0}
+];
+
+//QUIZ 2
+var testTwo = [
+	{question: 'Hvilken gass skulle egentlig ligge rundt hydrogengassen i Hindenburg som brannsikkerhet', answers: ['Helium', 'Argon', 'Neon', 'Xenon', 'Krypton'], correct: 0},
+	
+	{question: 'Hvor mange hjørner er det på et regulært ikosaeder?', answers: ['12', '9', '15', '10', '14', '11', '13', '16'], correct: 0},
+	
+	{question: 'Hva kalles læren om deformasjon- og flytegenskapene i seige væsker og plastiske masser?', answers: ['Reologi', 'Visilogi', 'Newtonologi', 'Elastolagi', 'Viskologi'], correct: 0},
+	
+	{question: 'Hvor mange primtall er det mellom 0 og 30?', answers: ['10', '11', '6', '9', '8', '7'], correct: 0},
+	
+	{question: 'Hva er dimensjonerende ute temperaturen i Trondheim i bygningsteknikken? I Celsius', answers: ['-19', '-13', '-21', '-17', '-15', '-11'], correct: 0},
+	
+	{question: 'Hva kalles komprimeringsalgoritmen som sorterer symboler etter antall forekomster for så å fordele dem i et binært tre med grenseverdier 0 og 1?', answers: ['Huffman-algoritmen', 'Smith–Waterman algoritmen', 'Needleman–Wunsch algoritmen', 'Hirschberg algoritmen', 'Schensted algoritmen', 'Kadanes algoritme'], correct: 0},
+	
+	{question: 'Radiusen til en sirkel øker med 50%. Hvor mange prosent øker sirkelens areal?', answers: ['125%', '175%', '150%', '75%', '50%', '100%'], correct: 0},
+
+	{question: 'Hvilken koeffisient bestemmes ut fra et Moody diagram', answers: ['Friksjon', 'Trykk', 'Varmeveksling', 'Luftmostand'], correct: 0},
+	
+	{question: 'Hvilken klassisk ligning kan du bruke til å finne krefter på en sylinder som står vertikalt i strømmende vann?', answers: ['Morisons ligning', 'Ricattis ligning', 'Pells ligning', 'Ishimoris ligning', 'Verhulsts ligning'], correct: 0},
+	
+	{question: 'cos^2(x)-sin^2(x) er det samme som?', answers: ['cos(2x)', 'cos(x/2)', 'tan(x/2)', 'tan(2x)', 'sin(2x)', 'sin(x/2)'], correct: 0},
+	
+	{question: 'Per, Pål og Espen har i gjennomsnitt 100 kroner. Per har like mye som Pål og Espen til sammen. Hvor mye har Per?', answers: ['150', '75', '125', '50', '200', '175', '100'], correct: 0},
+	
+	{question: 'Hva kalles teoremet som brukes til å regne ut annet arealmoment og treghetsmomentet for sammensatte tverrsnitt om ønsket akse?', answers: ['Parallellakseteoremet', 'Treghetsteoremet', 'Langevin teoremet', 'Friedmann teoremet', 'Vlasovs teorem'], correct: 0},
+	
+	{question: 'Hvis et materiale strekkes i en retning, har det en tendens til å komprimeres i en annen retning. Hva kalles dette fenomenet?', answers: ['Poissons fenomenet', 'Newton fenomenet', 'Hooke fenomenet', 'Euler fenomenet'], correct: 0},
+	
+	{question: 'Hvor mange satellitter må en GPS-mottaker ha kontakt med for å kunne gi en tredimensjonal posisjonsangivelse', answers: ['4', '2', '3', '5'], correct: 0},
+	
+	{question: 'Hva er integranden av 1/(cos(x)^2)', answers: ['tan(x)', '1/(sin(x)^2)', 'cos(x)^2/sin(x)^2', 'sin(x)', '1/tan(x)', '1/sin(x)'], correct: 0}
+];
+
+//QUIZ 3
 var testThree = [
 	{question: 'Hvor mye lengre unna jorden beveger månen seg hvert år?', answers:['0.038 m', '38 cm', '3.8 dm', '3.8m', '38m', '3.8*10^(-5) dm'], correct: 0},
 	
@@ -27,8 +94,7 @@ var testThree = [
 ];
 
 
-
-
+//QUIZ 4
 var testFour = [
 	{question: 'Hva er hex-tallet 3F i ti-tallsystemet?', answers:['63', '60', '61', '62', '67', '64', '65'], correct: 0},
 	
@@ -66,19 +132,19 @@ var testFour = [
 
 
 
-
+//QUIZ 5
 var testFive = [
 	{question: 'I maskinmekanikk, hva kalles spenningene mellom en tapp og en boring i en krympeforbindelse?', answers:['Pressmonn', 'Borremonn', 'Tappmonn', 'Trykkmonn', 'Pokemonn'], correct: 0},
 	
 	{question: 'Hvilket år startet nettsiden YouTube?', answers:['2005', '2001', '2002', '2003', '2004', '2008'], correct: 0},
 	
-	{question: 'Hva blir det binære tallet 1101010 i det vanlige titallsystemet?', answers:['106', '34', '76', '104', '78', '102']correct: 0},
+	{question: 'Hva blir det binære tallet 1101010 i det vanlige titallsystemet?', answers:['106', '34', '76', '104', '78', '102'], correct: 0},
 	
 	{question: 'I hvilke to land finner man de eneste pattedyrene som legger egg?', answers:['Australia og Indonesia', 'Brasil og Argentina', 'Fiji og Indonesia', 'Australia og New Zeleand', 'Thailand og Vietnam', 'Brasil og Columbia', 'Fiji og New Zealand'], correct: 0},
 	
 	{question: 'Hva kalles det å utføre en investering for å minimere eller fjerne risiko ved en annen investering?', answers:['Hedging', 'Shorting', 'Day-trading', 'LRB (Low Risk Buy)', 'Zeroing', 'Fonding'], correct: 0},
 	
-	{question: 'I astrofysikken, hva brukes parallaksemetoden til?', answers:['Å måle avstanden fra jorda til himmellegemer', 'Å måle hastigheten til himmellegemer', 'Å telle himmellegemer', 'Å regne ut flo og fjære', 'Å spå nordlys', 'Å forutsi planetenes baner rundt stjerner', '', '', ''], correct: 0},
+	{question: 'I astrofysikken, hva brukes parallaksemetoden til?', answers:['Å måle avstanden fra jorda til himmellegemer', 'Å måle hastigheten til himmellegemer', 'Å telle himmellegemer', 'Å regne ut flo og fjære', 'Å spå nordlys', 'Å forutsi planetenes baner rundt stjerner'], correct: 0},
 	
 	{question: 'I 2008 ble det sendt ca 210 milliarder epost hver dag. Hvor mange prosent av dette var spam?', answers:['70%', '30%', '40%', '50%', '60%', '80%', '90%'], correct: 0},
 	
@@ -94,9 +160,9 @@ var testFive = [
 	
 	{question: 'Fra og med 2009 er minstekravet til isolasjonstykkelse i yttervegger i boliger endret. Hva er det nye kravet?', answers:['25 cm', '10 cm', '15 cm', '20 cm', '30 cm', '35 cm'], correct: 0},
 	
-	{question: 'Hva er grenseverdien av sin(x)/x når x går mot null?', answers:['1', 'Den divergerer, fordi vi deler på null', '0', 'Uendelig', 'sin(x)', '', '', '', ''], correct: 0},
+	{question: 'Hva er grenseverdien av sin(x)/x når x går mot null?', answers:['1', 'Den divergerer, fordi vi deler på null', '0', 'Uendelig', 'sin(x)'], correct: 0},
 	
-	{question: 'Hvilken isotop av uran utgjør en høyere prosentvis andel i anriket uran enn i naturlig uran?', answers:['Uran-235', 'Uran-240', 'Uran-34', 'Uran-265', 'Uran-300'], correct: 0},
+	{question: 'Hvilken isotop av uran utgjør en høyere prosentvis andel i anriket uran enn i naturlig uran?', answers:['Uran-235', 'Uran-240', 'Uran-34', 'Uran-265', 'Uran-300'], correct: 0}
 	
 ];
 
--- script.js
+++ script.js
@@ -1,79 +1,50 @@
-//QUIZ 1
-var testOne = [
-	{question: 'Et fotballlag har vunnet 50% av de 20 siste kampene de har spilt så langt. Hvor mange kamper på rad må de vinne for å øke seierprosenten til 60%?', answers: ['5','8', '9', '6', '7', '10', '3', '4'], correct: 0},
-		
-	{question: 'Hvis du kjører til butikken i 20 km/t og tilbake i 30 km/t, hva er snitthastigheten din?', answers: ['24 km/t', '25 km/t', '26 km/t', '23 km/t','27 km/t', '22 km/t'], correct: 0},
-	
-	{question: 'Hva er vinkelsummen i en femkant?', answers: ['540 grader', '720 grader', '360 grader', '550 grader', '600 grader'], correct: 0},
-
-	{question: 'Hvilke to faser er det i duplex-stål', answers:['Austenitt & Ferritt', 'Ferritt & Martensitt', 'Bainitt & Perlitt', 'Bainitt & Martensitt', 'Perlitt & Ferritt', 'Sementitt & Austenitt'], correct: 0},
-	
-	{question: 'Hva er du i ferd med å kjøpe hvis man oppgir et DN og et PN nummer?', answers:['Rør', 'Flatstål', 'Bolt', 'Skruer', 'Bjelke'], correct: 0},
-	
-	{question: 'Hva menes med standardene 304L og 316L?', answers:['Rustfritt stål', 'Herdet stål', 'Aluminium', 'Svartstål', 'Titan'], correct: 0},
-
-	{question: 'Hva heter det i fysikken når en væske blir til gass som følge av at trykket i væsken synker til under damptrykket?', answers: ['Kavitasjon', 'Barotropi', 'Konveksjon', 'Adveksjon', 'Termal sirkulasjon'], correct: 0},
-	
-	{question: 'Hvilken spenning markerer skillet mellom elastisk og plastisk deformasjon?', answers: ['Flytespenning', 'Bøyespenning', 'Skjærspenning', 'Bruddspenning'], correct: 0},
-
-	{question: 'Hvis en 2000-watts vannkoker står på i 15 minutter. Hvor mange kilowattimer har den da brukt? Svar i kWh', answers:['0.5', '1', '0.8', '1.5', '0.3', '1.3'], correct: 0},
-	
-	{question: 'Hvilke av følgende er ikke et tegnsett?', answers:['IEEE', 'UTF-16', 'ASCII', 'UTF-8'], correct: 0},
-	
-	{question: 'Hva er navnet på en applikasjon som gjør om programmeringsspråk til et maskinspråk', answers:['Kompilator', 'Aritmetikk', 'Omformer', 'Interpreter'], correct: 0},
-	
-	{question: 'Hvor mange kvadratmeter er det i et mål?', answers:['1000', '100000', '100', '10000', '10'], correct: 0},
-	
-	{question: 'Hva skjer med en stålbjelke som varmes opp med forskjellig temperatur på over- og undersiden? Den...', answers:['Forlenges og bøyes', 'Forelenges', 'Krymper og bøyes', 'Krymper', 'Bøyes'], correct: 0},
-	
-	{question: 'Hvilken trigonometrisk funksjon beskriver forholdet mellom den hosliggende kateten og hypotenusen i en rettvinklet trekant', answers:['Cosinus', 'Arccosinus', 'Tangens', 'Arctanges', 'Sinus', 'Arcsinus'], correct: 0},
-	
-	{question: 'Hva er uttrykket for effektiviteten til en Carnot-varmemaskin?', answers:['1-(T_h/T_c)', '1/(T_c/T_h)', '1-(T_c/T_h)', '(T_h/T_c)-1', '1/(T_h/T_c)'], correct: 0}
-];
-
-//QUIZ 2
-var testTwo = [
-	{question: 'Hvilken gass skulle egentlig ligge rundt hydrogengassen i Hindenburg som brannsikkerhet', answers: ['Helium', 'Argon', 'Neon', 'Xenon', 'Krypton'], correct: 0},
-	
-	{question: 'Hvor mange hjørner er det på et regulært ikosaeder?', answers: ['12', '9', '15', '10', '14', '11', '13', '16'], correct: 0},
-	
-	{question: 'Hva kalles læren om deformasjon- og flytegenskapene i seige væsker og plastiske masser?', answers: ['Reologi', 'Visilogi', 'Newtonologi', 'Elastolagi', 'Viskologi'], correct: 0},
-	
-	{question: 'Hvor mange primtall er det mellom 0 og 30?', answers: ['10', '11', '6', '9', '8', '7'], correct: 0},
-	
-	{question: 'Hva er dimensjonerende ute temperaturen i Trondheim i bygningsteknikken? I Celsius', answers: ['-19', '-13', '-21', '-17', '-15', '-11'], correct: 0},
-	
-	{question: 'Hva kalles komprimeringsalgoritmen som sorterer symboler etter antall forekomster for så å fordele dem i et binært tre med grenseverdier 0 og 1?', answers: ['Huffman-algoritmen', 'Smith–Waterman algoritmen', 'Needleman–Wunsch algoritmen', 'Hirschberg algoritmen', 'Schensted algoritmen', 'Kadanes algoritme'], correct: 0},
-	
-	{question: 'Radiusen til en sirkel øker med 50%. Hvor mange prosent øker sirkelens areal?', answers: ['125%', '175%', '150%', '75%', '50%', '100%'], correct: 0},
-
-	{question: 'Hvilken koeffisient bestemmes ut fra et Moody diagram', answers: ['Friksjon', 'Trykk', 'Varmeveksling', 'Luftmostand'], correct: 0},
-	
-	{question: 'Hvilken klassisk ligning kan du bruke til å finne krefter på en sylinder som står vertikalt i strømmende vann?', answers: ['Morisons ligning', 'Ricattis ligning', 'Pells ligning', 'Ishimoris ligning', 'Verhulsts ligning'], correct: 0},
-	
-	{question: 'cos^2(x)-sin^2(x) er det samme som?', answers: ['cos(2x)', 'cos(x/2)', 'tan(x/2)', 'tan(2x)', 'sin(2x)', 'sin(x/2)'], correct: 0},
-	
-	{question: 'Per, Pål og Espen har i gjennomsnitt 100 kroner. Per har like mye som Pål og Espen til sammen. Hvor mye har Per?', answers: ['150', '75', '125', '50', '200', '175', '100'], correct: 0},
-	
-	{question: 'Hva kalles teoremet som brukes til å regne ut annet arealmoment og treghetsmomentet for sammensatte tverrsnitt om ønsket akse?', answers: ['Parallellakseteoremet', 'Treghetsteoremet', 'Langevin teoremet', 'Friedmann teoremet', 'Vlasovs teorem'], correct: 0},
-	
-	{question: 'Hvis et materiale strekkes i en retning, har det en tendens til å komprimeres i en annen retning. Hva kalles dette fenomenet?', answers: ['Poissons fenomenet', 'Newton fenomenet', 'Hooke fenomenet', 'Euler fenomenet'], correct: 0},
-	
-	{question: 'Hvor mange satellitter må en GPS-mottaker ha kontakt med for å kunne gi en tredimensjonal posisjonsangivelse', answers: ['4', '2', '3', '5'], correct: 0},
-	
-	{question: 'Hva er integranden av 1/(cos(x)^2)', answers: ['tan(x)', '1/(sin(x)^2)', 'cos(x)^2/sin(x)^2', 'sin(x)', '1/tan(x)', '1/sin(x)'], correct: 0}
-];
+var urlString = "url  ="+window.location.href;
+fetchLink = urlString.substring(urlString.length+1, urlString.indexOf('#'));
+var getQuiz = Number(fetchLink[1]);
+console.log('Found quiz: '+getQuiz);
 //{question: '', answers:['', '', '', '', '', '', '', '', ''], correct: 0},
 
-var quiz = testOne;
-
+function get(x){
+	return document.getElementById(x);
+}
+var quiz;
+switch(getQuiz){
+	case 0:
+		quiz=testOne;
+		break;
+	case 1:
+		get('bigLogo').innerHTML='Test Quiz 1';
+		quiz=testOne;
+		break;
+	case 2:
+		get('bigLogo').innerHTML='Test Quiz 2';
+		quiz=testTwo;
+		break;
+	case 3:
+		get('bigLogo').innerHTML='Test Quiz 3';
+		quiz=testThree;
+		break;
+	case 4:
+		get('bigLogo').innerHTML='Test Quiz 4';
+		quiz=testFour;
+		break;
+	case 5:
+		get('bigLogo').innerHTML='Test Quiz 5';
+		quiz=testFive;
+		break;
+	default:
+		alert('Found no valid quizzes, setting default to Test One');
+		get('bigLogo').innerHTML='Error mode: Test Quiz 1';
+		quiz = testOne;
+}
+ 
 //This is the original array for questions and answers:
 var ansArray = [];
 var qstArray = [];
 for (i=0;i<quiz.length;i++){
 	qstArray.push(quiz[i].question);
 	ansArray.push(quiz[i].answers[0]);
-	console.log('\nQ:'+qstArray[i]+'\nANS: '+ansArray[i]);
+	//console.log('\nQ:'+qstArray[i]+'\nANS: '+ansArray[i]);
 }
 
 
@@ -91,10 +62,6 @@ function shuffle(array) { //fisher-yates shuffle
         array[index] = temp;
     }
     return array;
-}
-
-function get(x){
-	return document.getElementById(x);
 }
 
 function generateLists(){
@@ -156,7 +123,7 @@ function checkCorrect(answer_list, shuffledArray){
 		}	
 	}
 	if (amountWrong==0){alert('Everything is correct!');forceActive();}
-	else{alert(amountWrong+' wrong answers!');}
+	else{alert(amountWrong+' wrong answers!\nYou can submit again in 30 seconds');}
 	console.log('Score: '+(answer_list.length-amountWrong)+'/'+answer_list.length);
 
 }
@@ -185,6 +152,7 @@ function submitAnswers(){
 	checkCorrect(ans_list, shuffledQuiz);
 }
 
+$(".wrapper").hide();
 var shuffledQuiz = shuffle(quiz);
 generateLists();
 setQuestions();
@@ -192,7 +160,9 @@ var amountWrong;
 var currentQuestion;
 var currentAns;
 
-//Submit-button. Add timer later.
+var timerValue = 30000;
+
+//Submit-button and timers.
 function hover() {
   $(".button").on("mouseenter", function() {
     return $(this).addClass("hover");
@@ -205,14 +175,33 @@ function hoverOff() {
 }
 function active() {
   $(".button").on("click", function() {
-	document.getElementByClassName("button").disabled = true;
 	submitAnswers();
+	$(this).fadeOut();
+
+
+	$('html, body').animate({scrollTop:120}, 400);//scroll speed
+	showButton();
+	showTimer();
+
+
   });
 }
 function forceActive() {//green submit button when it's called from 0 errors in checkCorrect.
   $(".button").on("click", function() {
 	 return $(this).addClass("active");
   });
+}
+function showButton(){
+	setTimeout(function() { 
+		   $(".button").show(); 
+	}, timerValue);
+}
+function showTimer(){
+	$(".wrapper").show();
+	setTimeout(function() { 
+		   $(".wrapper").hide(); 
+	}, timerValue);
+	
 }
 
 
